4a0f2a51bc6d35c1dc4ede0638b03aab8ffa5563
Ze-Limao <jeswitch.24@gmail.com>
2024-11-06 12:20:53 +0000

    fix: update topology and remove unused Cliente files

--- src/oNode/oNode.py
+++ src/oNode/oNode.py
@@ -6,18 +6,21 @@ class oNode:
     def __init__(self, interface_ip: str, server_ip: str):
         self.interface_ip = interface_ip
         self.server_ip = server_ip
+        self.server_port = 8080
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.interface_ip, 0))
         self.neighbors = []
 
     def connect(self):
-        self.socket.connect((self.server_ip, 8080))
-        print(f"Conectado ao servidor em {self.server_ip}:8080")
+        print(f"Conectado ao servidor em {self.server_ip}:{self.server_port}")
         try:
-            while True:                
+            while True:
+                # Recebe a mensagem do servidor
                 msg = Messages.receive(self.socket)
                 self.neighbors = Messages.decode_list(msg)
                 print(f"Vizinhos: {self.neighbors}")
+                
+                # Envia uma mensagem de confirmação ao servidor
                 Messages.send(self.socket, Messages.encode("OK"))
         except KeyboardInterrupt:
             Messages.send(self.socket, b'')
@@ -27,8 +30,8 @@ class oNode:
         self.socket.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Usage: python3 src.oNode.oNode <interface_ip> <server_ip>")
+    if len(sys.argv) != 3:
+        print("Usage: python3 -m src.oNode.oNode <interface_ip> <server_ip>")
         sys.exit(1)
 
     node = oNode(sys.argv[1], sys.argv[2])
