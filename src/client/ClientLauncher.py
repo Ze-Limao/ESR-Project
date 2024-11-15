3491e4a29a3c63ad4eb0e1558aaee03836662742
FilipeR13 <josefilipe.r.rodrigues13@gmail.com>
2024-11-15 18:34:25 +0000

    feat: thread safe map/string on client

--- src/client/ClientLauncher.py
+++ src/client/ClientLauncher.py
@@ -1,11 +1,13 @@
 import sys
 from tkinter import Tk
 from .Client import Client
-from typing import TypedDict, Dict
 from ..utils.messages import Messages_UDP
 from ..utils.config import ONODE_PORT, STREAM_PORT, RTP_PORT, SERVER_IP
+from ..utils.safemap import SafeMap
+from ..utils.safestring import SafeString
 import socket
 import time
+import threading
 
 class ClientLauncher:
 	def __init__(self, fileName: str):
@@ -16,9 +18,9 @@ class ClientLauncher:
 		self.root = Tk()
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 		self.socket.bind(('', ONODE_PORT))
-		self.points_of_presence: Dict[str, float] = {}
-		self.point_of_presence: str = None
-		
+		self.points_of_presence = SafeMap()
+		self.point_of_presence = SafeString()
+
 	def create_client(self) -> None:
 		app = Client(self.root, self.serverAddr, self.serverPort, self.rtpPort, self.fileName)
 		app.master.title("RTPClient")	
@@ -35,26 +37,30 @@ class ClientLauncher:
 		
 	def set_points_presence(self, points_of_presence: list):
 		for point in points_of_presence:
-			self.points_of_presence[point] = float('inf') 
+			self.points_of_presence.put(point, float('inf'))
+
+	def check_status_point_of_presence(self, point: str) -> None:
+		timestamp = time.time()
+		response = Messages_UDP.send_and_receive(self.socket, b'', point, ONODE_PORT)
+		if response is None:
+			print(f"Error: Could not get response from point of presence {point}")
+			self.points_of_presence.put(point, float('inf'))
+		else:
+			delay = time.time() - timestamp
+			self.points_of_presence.put(point, delay)
+			print(f"Point of presence {point} has latency {delay}")
+			current_point = self.point_of_presence.read()
+			if current_point == None:
+				self.point_of_presence.write(point)
+			else:
+				if self.points_of_presence.get(current_point) > delay:
+					self.point_of_presence.write(point)
 
 	def check_status_points_presence(self) -> None:
-		for point in self.points_of_presence.keys():
-			timestamp = time.time()
-			response = Messages_UDP.send_and_receive(self.socket, b'', point, ONODE_PORT)
-			if response is None:
-				print(f"Error: Could not get response from point of presence {point}")
-				self.points_of_presence[point] = float('inf')
-			else:
-				self.points_of_presence[point] = time.time() - timestamp
-				print(f"Point of presence {point} has latency {self.points_of_presence[point]}")
-				if self.point_of_presence == None:
-					self.point_of_presence = point
-				else:
-					if self.points_of_presence[self.point_of_presence] > self.points_of_presence[point]:
-						self.point_of_presence = point
-		print(f"Point of presence selected: {self.point_of_presence}")
-		print(self.points_of_presence)
-
+		for point in self.points_of_presence.get_keys():
+			threading.Thread(target=self.check_status_point_of_presence, args=(point,)).start()
+		print(self.points_of_presence.get_items())
+		print(self.point_of_presence.read())
 if __name__ == "__main__":
 	try:
 		fileName = sys.argv[1]
