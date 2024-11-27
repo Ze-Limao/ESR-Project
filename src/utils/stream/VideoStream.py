7a33491238528f56eeff23e3a691018a6daac330
Ze-Limao <jeswitch.24@gmail.com>
2024-11-27 14:21:21 +0000

    fix: enhance VideoStream class with reset functionality and filename handling

--- src/utils/stream/VideoStream.py
+++ src/utils/stream/VideoStream.py
@@ -2,6 +2,7 @@ import cv2
 
 class VideoStream:
     def __init__(self, filename):
+        self.filename = filename
         self.videoCapture = cv2.VideoCapture(filename)
         if not self.videoCapture.isOpened():
             raise IOError("Could not open video file.")
@@ -15,7 +16,14 @@ class VideoStream:
             _, encoded_frame = cv2.imencode('.jpg', frame)
             return encoded_frame.tobytes()
         else:
-            return None
+            self.reset()
+            ret, frame = self.videoCapture.read()
+            if ret:
+                self.frameNum += 1
+                _, encoded_frame = cv2.imencode('.jpg', frame)
+                return encoded_frame.tobytes()
+            else:
+                return None
 
     def frameNbr(self):
         """Get the current frame number."""
@@ -24,3 +32,9 @@ class VideoStream:
     def release(self):
         """Release the video capture object."""
         self.videoCapture.release()
+
+    def reset(self):
+        self.videoCapture.release()
+        self.videoCapture = cv2.VideoCapture(self.filename)
+        if not self.videoCapture.isOpened():
+            raise IOError("Could not reopen video file.")
