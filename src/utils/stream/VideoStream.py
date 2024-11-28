8901b45be74b2d9866ca29c99eb321636685764a
Ze-Limao <jeswitch.24@gmail.com>
2024-11-28 15:20:17 +0000

    refactor: streamline frame processing in VideoStream class

--- src/utils/stream/VideoStream.py
+++ src/utils/stream/VideoStream.py
@@ -8,20 +8,22 @@ class VideoStream:
             raise IOError("Could not open video file.")
         self.frameNum = 0
 
+    def processFrame(self, frame):
+        """Process the given frame: encode and return as bytes."""
+        self.frameNum += 1
+        _, encoded_frame = cv2.imencode('.jpg', frame)
+        return encoded_frame.tobytes()
+
     def nextFrame(self):
         """Get the next frame."""
         ret, frame = self.videoCapture.read()
         if ret:
-            self.frameNum += 1
-            _, encoded_frame = cv2.imencode('.jpg', frame)
-            return encoded_frame.tobytes()
+            return self.processFrame(frame)
         else:
             self.reset()
             ret, frame = self.videoCapture.read()
             if ret:
-                self.frameNum += 1
-                _, encoded_frame = cv2.imencode('.jpg', frame)
-                return encoded_frame.tobytes()
+                return self.processFrame(frame)
             else:
                 return None
 
